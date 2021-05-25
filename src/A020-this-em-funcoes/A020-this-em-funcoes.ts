fbda32391c9d3c607f37ef2b94be8502f5e4b2d0
Robson Soares <robson.santana.soares@gmail.com>
2021-05-25 10:50:22 -0300

    this em funções

--- src/A020-this-em-funcoes/A020-this-em-funcoes.ts
+++ src/A020-this-em-funcoes/A020-this-em-funcoes.ts
@@ -1,6 +1,6 @@
 export function funcao(this: Date, name: string, age: number): void {
   console.log(this);
-  console.log(name, age));
+  console.log(name, age);
 }
 
 funcao.call(new Date(), 'Rob', 30);
