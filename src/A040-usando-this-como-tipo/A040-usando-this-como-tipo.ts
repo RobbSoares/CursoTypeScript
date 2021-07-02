c2bddb4a30bfcc9baefe5eca0fb87fbd555cbaec
Robson Soares <robson.santana.soares@gmail.com>
2021-07-02 13:05:14 -0300

    Usando this como tipo

---
+++ src/A040-usando-this-como-tipo/A040-usando-this-como-tipo.ts
@@ -0,0 +1,57 @@
+export class Calculadora {
+  constructor(public numero: number) {}
+
+  add(n: number): this {
+    this.numero += n;
+    return this;
+  }
+
+  sub(n: number): this {
+    this.numero -= n;
+    return this;
+  }
+
+  mul(n: number): this {
+    this.numero *= n;
+    return this;
+  }
+
+  div(n: number): this {
+    this.numero /= n;
+    return this;
+  }
+}
+
+export class SubCalc extends Calculadora {
+  pow(n: number): this {
+    this.numero **= n;
+    return this;
+  }
+}
+
+const calc = new SubCalc(20);
+calc.add(5).mul(3).div(2).pow(2);
+console.log(calc);
+
+// Builder - GoF
+export class RequestBuilder {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Enviando dados via ${this.method} para ${this.url}`);
+  }
+}
+
+const request = new RequestBuilder();
+request.setUrl('http://www.google.com').setMethod('post').send();
