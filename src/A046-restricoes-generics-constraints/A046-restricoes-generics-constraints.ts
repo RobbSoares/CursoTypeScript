e1ce5b31870e704c7d6c76e1d13b2c4345f1b1ca
Robson Soares <robson.santana.soares@gmail.com>
2021-07-14 12:58:20 -0300

    Restrições em Generics (constraints)

--- src/A046-restricoes-generics-constraints/A046-restricoes-generics-constraints.ts
+++ src/A046-restricoes-generics-constraints/A046-restricoes-generics-constraints.ts
@@ -1,7 +1,7 @@
 type getKeyFn = <O, K extends keyof O>(object: O, key: K) => O[K];
 const getKey: getKeyFn = (object, key) => object[key];
 
-const animal = {
+export const animal = {
   name: 'Gato',
   race: 'Vira-lata',
   color: 'Yellow',
